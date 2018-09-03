04f8da7204f53ef5ccf9fa0786539dae1136327d
Donnie Heard <dhearddev@gmail.com>
2018-09-02 18:40:26 -0700

    added seeds.sql file for adding subjects

---
+++ models/seeds.sql
@@ -0,0 +1,22 @@
+use tutorific_db;
+
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Math",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Science",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Engineering",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Social Work",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Arts",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Fine Arts",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Philosophy",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+INSERT INTO subjects (subjectName,createdAt,updatedAt)
+VALUES ("Architecture",CURRENT_TIMESTAMP,CURRENT_TIMESTAMP);
+
+select * from reviews;
+select * from tutorposts;
+select * from subjects;
