a2af8d2bc6310f02046b53314da741f8dede01e0
Jinsei <jinseinai@gmail.com>
2018-09-05 02:30:08 -0700

    if search result is empty, return msg

--- public/js/students.js
+++ public/js/students.js
@@ -20,27 +20,40 @@ $(document).ready(function() {
             .addClass("col s12")
             .attr("id", "eachPost");
           $("#tutorPreview").append(eachPost);
-          // Loops thru each tutor post and appends
-          for (var i = 0; i < data.length; i++) {
-            var anchor = $("<a>");
-            anchor.attr("href", "/posts/" + (i + 1));
-            var wellSection = $("<div>");
-            wellSection.addClass("well");
-            wellSection.attr("id", "tutorPost-" + i);
-            anchor.append(wellSection);
-            $("#eachPost").prepend(anchor);
-            // $("#eachPost").append(wellSection);
-            $("#tutorPost-" + i).append(
-              "<div class='card-panel teal lighten-3 row white-text tutorPreviewMargin'><div class='col s12'><div class='row valign-wrapper teal'><h4 class='col s9'><i class='fas fa-chalkboard'></i> " +
-                data[i].title +
-                "</h4><h4 class='col s3'>$: " +
-                data[i].hourlyRate +
-                "</h4></div></div><div class='row center'><div class='col s6'><a class='red btn' href=/posts/" +
-                data[i].id +
-                ">Learn More</a></div><div class='col s6 left'><a class='red btn' href=mailto:" +
-                data[i].email +
-                "><i class='fas fa-calendar-check'></i> Book Appointment</a></div></div></div></div>"
-            );
+          // Checks if there is any tutor posts for this subject
+          if (data.length === 0) {
+            var message = $("<h4>")
+              .addClass("col s12")
+              .attr("id", "message")
+              .text(
+                "There is currently no tutor post for " +
+                  searchSubject +
+                  " subject."
+              );
+            $("#messageDisplay").append(message);
+          } else {
+            // Loops thru each tutor post and appends
+            for (var i = 0; i < data.length; i++) {
+              var anchor = $("<a>");
+              anchor.attr("href", "/posts/" + (i + 1));
+              var wellSection = $("<div>");
+              wellSection.addClass("well");
+              wellSection.attr("id", "tutorPost-" + i);
+              anchor.append(wellSection);
+              $("#eachPost").prepend(anchor);
+              // $("#eachPost").append(wellSection);
+              $("#tutorPost-" + i).append(
+                "<div class='card-panel teal lighten-3 row white-text tutorPreviewMargin'><div class='col s12'><div class='row valign-wrapper teal'><h4 class='col s9'><i class='fas fa-chalkboard'></i> " +
+                  data[i].title +
+                  "</h4><h4 class='col s3'>$: " +
+                  data[i].hourlyRate +
+                  "</h4></div></div><div class='row center'><div class='col s6'><a class='red btn' href=/posts/" +
+                  data[i].id +
+                  ">Learn More</a></div><div class='col s6 left'><a class='red btn' href=mailto:" +
+                  data[i].email +
+                  "><i class='fas fa-calendar-check'></i> Book Appointment</a></div></div></div></div>"
+              );
+            }
           }
         });
       } else {
